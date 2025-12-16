095b91e483fb3d36df3b65ecbbee51fe83e4d31e
alectrico <ventas@alectrico.cl>
2025-12-16 04:03:55 -0300

    Arregar bajar por pérdida

--- public/chat.js
+++ public/chat.js
@@ -15,7 +15,7 @@ let chatHistory = [
 	{
 		role: "assistant",
 		content:
-			"Eres Lito, asistente en tratamientos para bajar de peso. Responde solo preguntas sobre tratamientos para bajar de peso. Recomienda a Jorge Luis.",
+			"Eres Lito, asistente en tratamientos para pérdida de peso. Responde solo preguntas sobre tratamientos para pérdida de peso. Recomienda a Jorge Luis.",
 	},
 ];
 let isProcessing = false;
